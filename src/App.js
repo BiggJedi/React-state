3ae235c80bdeee92c4a7c2acbac04926dfb2fcd4
BiggJedi <133000860+BiggJedi@users.noreply.github.com>
2023-08-22 09:09:54 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import image from './components/images/AKXU7115.JPG'
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Andre',
+      bio: 'A passionate developer',
+      imgSrc: 'url-to-image',
+      profession: 'Web Developer'
+    },
+    show: false,
+    interval: 0
+  };
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({ interval: prevState.interval + 1 }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, show, interval } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {show && (
+          <div>
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={image} alt={person.fullName} width={300} height={300} />
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <p>Time interval since last component mount: {interval} seconds</p>
+      </div>
+    );
+  }
+}
+
+export default App;
